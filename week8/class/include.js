5787d1d6b39c482063bb853d015bd66e3795cba9
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2023-10-30 22:46:47 +0700

    Add files via upload

---
+++ week8/class/include.js
@@ -0,0 +1,11 @@
+
+const animals = ['ant', 'dogs', 'cats', 'bird']
+const message = 'Practices make perfect'
+
+//string includes - includes substring (case sensitive)
+console.log(message.includes('ice')) //true
+console.log(message.includes('make')) //true
+//array includes - includes string in array elements (case sensitive)
+console.log(animals.includes('ats')) //false
+console.log(animals.includes('cats')) //true
+
