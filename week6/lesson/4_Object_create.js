279e6d89a7f44e8ceb4d540973ccdf801048905b
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2023-12-14 01:02:23 +0700

    Delete week6 directory

--- week6/lesson/4_Object_create.js
+++
@@ -1,9 +0,0 @@
-//const pp1 = new Person(1001 ,"Adam", "Jo")
-const pp1 = Object.create(p1); // copy obj p1 
-console.log(p1); // Person { id: 1001, firstname: 'Adam', lastname: 'Jo' } 
-console.log(pp1); // Person {}
-console.log(pp1.id); // 1001
-console.log(Object.prototype.isPrototypeOf(pp1)); //true
-console.log(Person.prototype.isPrototypeOf(pp1)); //true
-pp1.hobbies = ["reaing", "pooping"];
-console.log(pp1); // Person { hobbies: [ 'reaing', 'pooping' ] }
--- week6/lesson/test.js
+++
@@ -1,9 +0,0 @@
-let name ={
-    firstname: "Apisara"
-}
-function hey(nameObj){
-
-    const key1 = Object.keys(nameObj);
-}
-
-console.log(hey(name));
